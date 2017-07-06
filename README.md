3b0ef750438caece8164c394ada1cbcf4feb4076
Seiya KUBO <s.kubo@i3design.co.jp>
2017-07-06 10:11:35 +0900

    init

---
+++ README.md
@@ -0,0 +1,16 @@
+# Homebrew pecl_http-1
+
+## Requirements
+
+* [Homebrew](https://github.com/Homebrew/brew)
+* [Homebrew PHP](https://github.com/Homebrew/homebrew-php)
+* Yosemite, El Capitan, and Sierra. Untested everywhere else.
+
+
+## Installation
+
+Run the following in your command-line:
+
+```console
+$ homebrew tap seiyakubo/homebrew-php-http1
+```
---
+++ php53-http@1.rb
@@ -0,0 +1,34 @@
+require File.expand_path("/usr/local/Homebrew/Library/Taps/homebrew/homebrew-php/Abstract/abstract-php-extension", __FILE__)
+
+class Php53HttpAT1 < AbstractPhp53Extension
+  init
+  desc "This HTTP extension aims to provide a convenient and powerful set of functionality for one of PHPs major applications."
+  homepage "https://pecl.php.net/package/pecl_http"
+  url "https://github.com/m6w6/ext-http/archive/RELEASE_1_7_6.tar.gz"
+  sha256 "b0012dda19eee9c5af0280d30fbbb7bd9e38cb29225766764689830d90109283"
+  revision 1
+
+  head "https://github.com/m6w6/ext-http.git"
+
+  depends_on "curl"
+  depends_on "libevent"
+
+  def config_filename
+    "ext-http@1.ini"
+  end
+
+  def extension
+    "http"
+  end
+
+  def install
+    safe_phpize
+    system "./configure", "--prefix=#{prefix}",
+                          phpconfig,
+                          "--with-libevent-dir=#{Formula["libevent"].opt_prefix}",
+                          "--with-curl-dir=#{Formula["curl"].opt_prefix}"
+    system "make"
+    prefix.install "modules/http.so"
+    write_config_file if build.with? "config-file"
+  end
+end
